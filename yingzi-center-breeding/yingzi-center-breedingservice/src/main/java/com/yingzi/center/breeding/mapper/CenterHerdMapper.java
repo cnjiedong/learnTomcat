b7542be7ab7e68d690acbc4c2f68838c6149f32c
username <useremail>
2018-05-13 00:31:22 +0800

    learn tomcat7 and spring boot

--- yingzi-center-breeding/yingzi-center-breedingservice/src/main/java/com/yingzi/center/breeding/mapper/CenterHerdMapper.java
+++
@@ -1,9 +0,0 @@
-package com.yingzi.center.breeding.mapper;
-import com.dtyunxi.huieryun.ds.BaseMapper;
-import com.yingzi.center.breeding.eo.CenterEventEo;
-import com.yingzi.center.breeding.eo.CenterHerdEo;
-
-
-public interface CenterHerdMapper extends BaseMapper<CenterHerdEo> {
-
-}
